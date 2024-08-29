1d1bb68bd6ddd9dd56f699cd091dad9459bbe2e8
Adnan Ahmad <cyberlyadnan@gmail.com>
2024-08-29 14:44:53 +0530

    remove console from files

--- src/components/Hooks/usegenerateContent.js
+++ src/components/Hooks/usegenerateContent.js
@@ -8,7 +8,6 @@ const useGenerateContent = (searchQuery, triggerSearch) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log(process.env.REACT_APP_GEMINI_API_KEY)
     if (!searchQuery || !triggerSearch) return;
 
     const fetchData = async () => {
