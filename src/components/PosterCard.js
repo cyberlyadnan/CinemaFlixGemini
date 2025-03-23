d597fc22d636152713d82cc07aa15ad1a0f4691e
Adnan Ahmad <cyberlyadnan@gmail.com>
2025-03-23 13:42:04 +0530

    fixed small issue

--- src/components/PosterCard.js
+++ src/components/PosterCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PosterCard = ({ imageId }) => {
   const imageUrl = `https://image.tmdb.org/t/p/w500${imageId}`;
-
+  if (!imageId) return null;
   return (
     <div className=" mr-4 mb-12 ">
       <img src={imageUrl} alt="Movie Poster" className="min-w-[130px] sm:min-w-[150px] lg:min-w-[180px] w-full h-auto rounded-md cursor-pointer hover:shadow-white" />
